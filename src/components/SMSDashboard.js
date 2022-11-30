8426e60d583c0a28368968a27dd1d3af7236b9e8
Yamikani Phiri <yamikanidj@gmail.com>
2022-11-30 16:13:41 +0200

    daft data from get request

--- src/components/SMSDashboard.js
+++ src/components/SMSDashboard.js
@@ -11,9 +11,29 @@ class SMSDashboard extends Component{
 
     }  
     async componentDidMount(){  
+        // get use groupm data
+        // github: omar-bakhsh
+
+
+        let num=993855050;
+    
+        let msg= "ejjeje";
+    
+        let name= "thuw";
+        
+        var win = window.open(`https://wa.me/${num}?text=I%27m%20api%20msg%20hello%20${name}%20friend%20${msg}`, '_blank');
+        // win.focus();
+
         let usersData = await Api.getUserGroup() 
+
         for (let user of usersData.users) {
+            // get ou for every user
             let usersOuData = await Api.getUserOU(user.id) 
+            // TODO: add funtion/ loop  to get the programs dynamically
+            let programId ="sXzdrtXMink"
+            let evenData = await Api.getAnalytictsevenData(programId,usersOuData.organisationUnits[0].id) 
+            debugger
+            
 
 
         }
--- src/dhis/api.js
+++ src/dhis/api.js
@@ -1,18 +1,25 @@
 
 import {baseUrl, basicAuth} from './config';
 class Api {
+  //get use in a group
   getUserGroup(){
     let payload = "userGroups/FUf0ROq4wYr.json?fields=id,name,users[id,name]"
     
     return getJSON(payload)
   }
+  //get user ou based on id
   getUserOU(userId){
-    let payload = "users/"+userId+".json?fields=organisationUnits[id,name]"
-    
+    let payload = "users/"+userId+".json?fields=organisationUnits[id,name]"    
     return getJSON(payload)
   }
+  //get analytics data
+  getAnalytictsevenData(programId,OUId){
+    let payload = "analytics/events/aggregate/"+programId+".json?dimension=ou:"+OUId+"&dimension=pe:THIS_YEAR&filter=e3vz0w6dU7o.RSr9YoqpPS7&stage=e3vz0w6dU7o&displayProperty=NAME&outputType=EVENT" 
+    return getJSON(payload)
+  }
+  
 }
-
+// https://ccdev.org/chistest/api/29/
 // Send GET request
 function getJSON(path) {
     return fetch( `${baseUrl}/${path}`, {
